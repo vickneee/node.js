3044aa4dddebdae234f605a6320b6dc16fd9904b
vickneee <victoria.vavulina@outlook.com>
2023-05-18 14:30:27 +0300

    Initial Commit

--- create-table/MyFunctions.js
+++ create-table/MyFunctions.js
@@ -1,57 +1,41 @@
-var mysql = require('mysql');
-var html =`<!DOCTYPE html>
-<html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
-<h1>Hello from Server</h1>
-<p>It is nice to code in JS & Node</p>
-</body>
-</html>
-`
-exports.html = html
-exports.myDateTime = function () {
-	return Date();
-};
-
 function CreateTable(r) {
-	var table
-	var style =
-`<style>
+	let table;
+	const style =
+		`<style>
 #table {border-collapse: collapse;width: 100%;}
 #th, #td {text-align: left;padding: 8px;border: 1px solid #dddddd;}
 #tr:nth-child(even){background-color: #D6EEEE;}
-</style>`
-	var tab = '\t'
-	var eol = '\n'
-	var firstpart = `<html>${eol}<table id="table">`
-	var lastpart = `${eol}</table>${eol}</html>`
-	var trs = `${eol}<tr id="tr">${eol}${tab}`
-	var tre = `${eol}</tr>`
-	var ths = '<th id="th">'
-	var the = '</th>'
-	var tds = '<td id="td">'
-	var tde = '</td>'
-	var header = ""
-	var Keys = Object.keys(r[0]);
-	var kl = Keys.length 
-	var Rows = Object.values(r);
-	var rl = Rows.length
+</style>`;
+	let tab = '\t'
+	let eol = '\n'
+	let firstPart = `<html lang="en">${eol}<table id="table">`
+	let lastPart = `${eol}</table>${eol}</html>`
+	let trs = `${eol}<tr id="tr">${eol}${tab}`
+
+	let tre = `${eol}</tr>`
+	let ths = '<th id="th">'
+	let the = '</th>'
+	let tds = '<td id="td">'
+	let tde = '</td>'
+	let header = ""
+	let Keys = Object.keys(r[0]);
+	let kl = Keys.length
+	let Rows = Object.values(r);
+	let rl = Rows.length
 	header = header + trs
-	for (i = 0; i < kl; i++) {
+	for (let i = 0; i < kl; i++) {
 		header = header + ths + Keys[i] + the
 	}
 	header = header + tre
-	var rows = ""
-	for (i = 0; i < rl; i++) {
+	let rows = ""
+	for (let i = 0; i < rl; i++) {
 		rows = rows + trs;
-		for (j = 0; j < kl; j++) {
+		for (let j = 0; j < kl; j++) {
 			rows = rows + tds + Rows[i][Keys[j]] + tde
 		}
 		rows = rows + tre;
 	}
-	table =`${firstpart}${style}${header}${rows}${lastpart}`
+	table =`${firstPart}${style}${header}${rows}${lastPart}`
 	return table
 }
 exports.CreateTable = CreateTable
