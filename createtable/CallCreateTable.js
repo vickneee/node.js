e32b102c9ff98e6097bbbd296a135e4ab2acc98f
vickneee <victoria.vavulina@outlook.com>
2023-05-18 13:41:58 +0300

    Initial Commit

--- createtable/CallCreateTable.js
+++
@@ -1,5 +0,0 @@
-var mf = require('./MyFunctions');
-r = [{Nimi:'Pekka', Ika:46}, {Nimi:'Tiina', Ika:25}, {Nimi:'Annika', Ika:34}, {Nimi:'Hannu', Ika:57}]
-var html = mf.CreateTable(r)
-console.log(html)
-process.exit(0)
